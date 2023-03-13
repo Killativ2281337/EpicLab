02328f2ee2914b91c1edab765e36ddc592b7d679
vboxuser <vboxuser@ubuntu3.myguest.virtualbox.org>
2023-03-13 17:49:15 +0200

    first commit

---
+++ engine.cpp
@@ -0,0 +1,21 @@
+#include "engine.h"
+Engine::Engine(){
+    init();
+
+}
+
+void Engine::init(){
+    window.create(sf::VideoMode(1260,720),"Game");
+}
+
+void Engine::loop() {
+    while (window.isOpen()){
+        Event event;
+        while (window.pollEvent(event)){
+            if (event.type == Event::Closed){
+                window.close();
+            }
+        }
+    }
+    
+}
